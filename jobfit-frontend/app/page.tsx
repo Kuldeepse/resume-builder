e2de05cd124307946169aefbb30a1279001896d1
Kuldeepse <37650486+Kuldeepse@users.noreply.github.com>
2026-05-22 20:39:45 +0100

    Update page.tsx

--- jobfit-frontend/app/page.tsx
+++ jobfit-frontend/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
       const timeoutId = setTimeout(() => controller.abort(), 90000);
       
       // ✅ Explicitly targets your backend instance AND the /build-resume route endpoint
-      const res = fetch("https://resume-builder-backend-ph7b.onrender.com", { //  CORRECT SUBDOMAIN
+      const res = fetch("https://onrender.com", { //  CORRECT SUBDOMAIN
         method: 'POST', 
         body: formData, 
         signal: controller.signal 
