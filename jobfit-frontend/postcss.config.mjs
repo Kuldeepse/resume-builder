5d14c4aadb1c1562e1d03b276dbeb1b6f1405c7f
Kuldeepse <37650486+Kuldeepse@users.noreply.github.com>
2026-05-22 13:10:36 +0100

    Update postcss.config.mjs

--- jobfit-frontend/postcss.config.mjs
+++ jobfit-frontend/postcss.config.mjs
@@ -1,7 +1,13 @@
-const config = {
+//const config = {
+  //plugins: {
+  //  "@tailwindcss/postcss": {},
+ // },
+///};
+
+///export default config;
+export default {
   plugins: {
-    "@tailwindcss/postcss": {},
+    '@tailwindcss/postcss': {},
   },
 };
 
-export default config;
